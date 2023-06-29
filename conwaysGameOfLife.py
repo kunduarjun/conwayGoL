f6dbf370d79ee2c52477dafb66a425ce90c81168
Arjun Kundu <kunduarjun02@gmail.com>
2023-06-29 00:42:32 -0300

    Progress

--- conwaysGameOfLife.py
+++ conwaysGameOfLife.py
@@ -3,3 +3,17 @@
 # I am simply following along to his YouTube video, as I am looking to populate my GitHub with more reposiitories.
 # I will add comments onto this program, in order to demonstrate that I understand what was discussed in the video.
 # This will also demonstrate that I understand the requirements to program this game. 
+
+# Rules of the game:
+# 1) Living cells with 2 or 3 neighbours stay alive.
+# 2) Dead cells with 3 living neighbours become alive again.
+# 3) All other cells become or stay dead. 
+
+# Press Ctrl-C to stop the simulation. 
+
+import copy, random, sys, time
+
+# Set up some constants within the program
+WIDTH = 45 # Width of the cell grid.
+HEIGHT = 25 # Height of the cell grid. 
+
