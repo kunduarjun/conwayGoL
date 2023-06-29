c4e61e4276b8896a40dc0bfd24caea3dd0aceb5a
Arjun Kundu <kunduarjun02@gmail.com>
2023-06-29 00:25:50 -0300

    Added comments

--- conwaysGameOfLife.py
+++ conwaysGameOfLife.py
@@ -1,2 +1,2 @@
 # This is a program created by Al Sweigart, who is the author of "Automating the Boring Stuff with Python."
-# Al Sweigart demonstrated how to create this program on 
+# Al Sweigart demonstrated how to create this program through his YouTube channel
